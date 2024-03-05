778cf01fad6ef4b5fafd2c62ae9cf8b2d6883e71
JohnMosc <150018784+JohnMosc@users.noreply.github.com>
2024-03-05 19:18:02 +1000

    first

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
+const container = document.querySelector(".page__content");
+const cardListContainer = container.querySelector(".places__list"); //куда вставляем карточки
 
-// @todo: DOM узлы
+// Функция создания карточки
 
-// @todo: Функция создания карточки
+function createCard(cardContent, deleteFunction) {
+  const templateCard = document.querySelector("#card-template").content;
+  const itemCard = templateCard.querySelector(".places__item").cloneNode(true);
 
-// @todo: Функция удаления карточки
+  itemCard.querySelector(".card__image").src = cardContent.link;
+  itemCard.querySelector(".card__title").textContent = cardContent.name;
 
-// @todo: Вывести карточки на страницу
+  const deleteButton = itemCard.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteFunction);
+
+  return itemCard;
+}
+
+// Функция удаления карточки
+
+function deleteCard(evt) {
+  evt.target.closest(".places__item").remove();
+}
+
+// Выводит карточки на страницу
+
+initialCards.forEach(function (itemCard) {
+  const card = createCard(itemCard, deleteCard);
+  cardListContainer.append(card);
+});
